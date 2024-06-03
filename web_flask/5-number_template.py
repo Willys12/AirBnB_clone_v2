c5a1a56e3fb7542f332f8dcf3832816ef919060d
Willys12 <ben.willys92@gmail.com>
2024-06-03 15:04:41 +0300

    Improve engines

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Launch the web application with two routes.
+"""
+Launch the web application with two routes.
 """
 
 from flask import Flask, render_template
@@ -8,21 +9,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    """Provide a string in response to a queried route.
+    """
+    Provide a string in response to a queried route.
     """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb')
 def hbnb():
-    """Return string when route queried
+    """
+    Return string when route queried
     """
     return 'HBNB'
 
 
 @app.route('/c/<text>')
 def c_is_fun(text):
-    """Return reformatted text
+    """
+    Return reformatted text
     """
     return 'C ' + text.replace('_', ' ')
 
@@ -30,7 +34,8 @@ def c_is_fun(text):
 @app.route('/python/')
 @app.route('/python/<text>')
 def python_with_text(text='is cool'):
-    """Reformat the text based on the presence
+    """
+    Reformat the text based on the presence
     of an optional variable.
     """
     return 'Python ' + text.replace('_', ' ')
@@ -38,14 +43,16 @@ def python_with_text(text='is cool'):
 
 @app.route('/number/<int:n>')
 def number(n=None):
-    """Permit the request if the path variable is a valid integer.
+    """
+    Permit the request if the path variable is a valid integer.
     """
     return str(n) + ' is a number'
 
 
 @app.route('/number_template/<int:n>')
 def number_template(n):
-    """Obtain the template for the request.
+    """
+    Obtain the template for the request.
     """
     path = '5-number.html'
     return render_template(path, n=n)
